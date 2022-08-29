8cd8c0183f95a1999913c6c1b36fb1a0d32dc156
Slawek01~ <lilcrunker@gmail.com>
2022-08-29 23:24:55 +0200

    added next button function and animate progress bar

---
+++ script.js
@@ -0,0 +1,44 @@
+const formPages = document.querySelectorAll('.page')
+const steps = document.querySelectorAll('.step')
+const progressBar = document.querySelector('.progress-bar')
+const prevBtn = document.querySelector('.btn-prev')
+const nextBtn = document.querySelector('.btn-next')
+
+let currentStep = 1
+
+const handleNextBtn = () => {
+	currentStep++
+
+	if (currentStep > steps.length) {
+		currentStep = steps.length
+	}
+    handleProgressBar()
+}
+const handlePrevBtn = () => {
+	currentStep--
+
+	if (currentStep < 1) {
+		currentStep = 1
+	}
+    handleProgressBar()
+}
+
+const handleProgressBar = () => {
+    steps.forEach((step, index)=> {
+        if(index < currentStep){
+            step.classList.add('active-step')
+        } else{
+            step.classList.remove('active-step')
+        }
+    })
+
+    const activeSteps = document.querySelectorAll('.active-step')
+    console.log(((activeSteps.length -1) / (steps.length -1)) * 100 + '%');
+    progressBar.style.width = ((activeSteps.length -1) / (steps.length -1)) * 100 + '%'
+
+    
+
+}
+
+nextBtn.addEventListener('click', handleNextBtn)
+prevBtn.addEventListener('click', handlePrevBtn)
